7e8f50f973252fafc86bb5d93724cd98798ec39d
OdileOwl <33974380+OdileOwl@users.noreply.github.com>
2018-02-14 22:18:56 +0800

    Add files via upload

---
+++ initinfo/initinfo.js
@@ -0,0 +1,29 @@
+// pages/initinfo/initinfo.js
+Page({
+  data: {
+    array: ['QQ', '微信号', '手机号', '邮箱'],
+    index: 0,
+
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  pickerConfirm: function (e) {
+
+    this.setData({
+      chosen: e.detail.value
+    })
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function (e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
+    })
+  }
+})
---
+++ login/login.js
@@ -0,0 +1,13 @@
+// pages/login/login.js
+Page({
+  data: {
+    focus: false,
+    inputValue: ''
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+})
