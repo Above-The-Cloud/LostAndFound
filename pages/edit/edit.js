682b6a27d15ba5c84f84fafff2df7717d2a35c39
HimsenLee <33974037+HimsenLee@users.noreply.github.com>
2018-03-19 09:54:39 +0800

    Update edit.js

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,5 +1,3 @@
-//index.js
-//获取应用实例
 const app = getApp()
 var serverName = app.globalData.serverName
 
@@ -65,42 +63,6 @@ Page({
   bindViewTap: function () {
 
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
 
   //选择多张图片
   uploadimg: function (data) {
@@ -142,19 +104,7 @@ Page({
   },
 // 选择照片
   choose_picture: function(){
-    // var that = this;
-    // wx.chooseImage({
-    //   count: 3, // 默认9  
-    //   sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
-    //   sourceType: 'album', // 可以指定来源是相机 
-    //   success: function (res) {
-    //     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
-    //     that.setData({
-    //       tempFilePaths: res.tempFilePaths,
-    //       image_exist: 3
-    //     })
-    //   }
-    // })
+
   //这里是选取图片的方法
       var that = this,
         　　　　　　pics = this.data.pics;
