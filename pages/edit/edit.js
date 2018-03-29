7f64028647509024788dee162f25db65f5f0cacd
HimsenLee <33974037+HimsenLee@users.noreply.github.com>
2018-03-29 22:41:19 +0800

    发布界面bug修复
    FOUND消息清空
    分类默认修改

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -16,21 +16,21 @@ Page({
     publish_id: -1,
     image_exist: 0,
     user_id: 10152150127,
-    array: ['所有', '校园卡', '雨伞', '钱包'],
-    pflag: 0,
     // 单选框
     items: [
       { name: 'lost', value: 'LOST', checked: 'true' },
       { name: 'found', value: 'FOUND' },
     ],
     //图片路径
-    tempFilePaths: [],
+    tempFilePaths:null,
     //分类按钮
     showModalStatus: false,
     //导航栏
     navbar: ['LOST', 'FOUND'],
     currentTab: 0,
-    imageList: [],
+    array: ['所有', '校园卡', '雨伞', '钱包'],
+    pflag: 0,
+    imageList: [], 
     tvalue:'',
   },
   powerDrawer: function (e) {
@@ -41,13 +41,13 @@ Page({
     this.setData({
       currentTab: e.currentTarget.dataset.idx
     })
-  },
+  },  
   //单选框触发函数
   radioChange: function (e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
-
+    
   },
-  //
+   //
   stateswitch: function (e) {
     console.log('LLL')
     if (this.data.currentTab == 0) {
@@ -63,9 +63,9 @@ Page({
     console.log(this.data.listofitem)
     console.log('FFF')
     this.setData({
-      tvalue: '',
-      imageList:'',
-      pflag:'0',
+      tvalue:'',
+      imageList:[],
+      pflag:0,
     })
   },
 
@@ -79,6 +79,7 @@ Page({
     wx.chooseImage({
       count: 3,
       success: function (res) {
+        console.log('chooseimage.......')
         console.log(res)
         that.setData({
           imageList: res.tempFilePaths
@@ -88,49 +89,75 @@ Page({
   },
   previewImage: function (e) {
     var current = e.target.dataset.src
-
+    console.log('current')
+    console.log(current)
     wx.previewImage({
       current: current,
       urls: this.data.imageList
     })
   },
 
-  bindPickerChange: function(e) {
+  bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       pflag: e.detail.value
     })
   },
-  bindDateChange: function(e) {
+  bindDateChange: function (e) {
     this.setData({
       date: e.detail.value
     })
   },
-  bindTimeChange: function(e) {
+  bindTimeChange: function (e) {
     this.setData({
       time: e.detail.value
     })
   },
-  
-  // onShow: function () {
-  //  this.setData({
-  //    tvalue: '',
-  //    imageList:'',
-  //    pflag:'0',
-  //  })
-  // },
+
+
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
     console.log(e)
     var that = this;
     var formData = e.detail.value;
+
+    var user_id = wx.getStorageSync('user_id')
+    var type_t = this.data.listofitem
+    var category = this.data.pflag
+    var title = ''
+    var msg = e.detail.value.input
+    var imagesPaths = this.data.imageList
+    console.log("imagelist..........")
+    console.log(this.data.imageList)
+    console.log(imagesPaths)
+    //在此调用uploadAll接口
+    this.uploadAll(user_id, type_t, category, title, msg, imagesPaths)
+
+    //跳转到主页
+    wx.switchTab({
+      url: '../index/index',
+      success: function (e) {
+        var page = getCurrentPages().pop();
+        if (page == undefined || page == null) return;
+        setTimeout(function () {
+          page.onLoad();
+        }, 2000);  
+        
+      }
+    })  
+  },
+
+  //imagesPaths图片路径数组
+  uploadAll: function (user_id, type_t, category, title, msg, imagesPaths) {
+    var publish_id=null;
     wx.request({
-      url: serverName + '/edit.php',
+      url: serverName + '/edit/edit.php',
       data: {
-        user_id: that.data.user_id,
-        type_t: e.detail.value.type,
-        title: '',
-        msg: e.detail.value.input,
+        user_id: user_id,
+        type_t: type_t,
+        category: category,
+        title: title,
+        msg: msg,
         image_exist: 0,
 
       },
@@ -139,48 +166,31 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        //console.log('sucess-----------------')
-        //console.log(res)
-        //console.log('-----------------------')
-        //console.log(res.data)
-        //console.log('sucess-----------------')
-        that.setData({
-          publish_id: res.data.max_pid
-
-        })
+          publish_id=res.data
         console.log('当前数据库返回的publish_id')
-        console.log(that.data.publish_id)
-        if (that.data.image_exist == 1) {
-          //console.log(that.data)
+        console.log(publish_id)
+        for (var path in imagesPaths){
+          console.log(imagesPaths[path])
           wx.uploadFile({
-            url: serverName + '/upload.php',
-            filePath: that.data.tempFilePaths[0],
+            url: serverName + '/edit/upload.php',
+            filePath: imagesPaths[path],
             name: "file",
             formData: {
-              publish_id: that.data.publish_id
+              publish_id: publish_id
             },
             success: function (res) {
               console.log('图片上传完成！')
               console.log(res)
-              //var data = res.data
-              //do something
+            
+            },
+            fail: function (err) {
+              console.log(err)
             }
           })
         }
+        
       }
     })
-    wx.switchTab({
-      url: '../index/index',
-      success: function (e) {
-        var page = getCurrentPages().pop();
-        if (page == undefined || page == null) return;
-        setTimeout(function () {
-          page.onLoad();
-        }, 2000);
-
-      }
-    })
-  },
-
+  }
 
 })
